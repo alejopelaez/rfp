07f00c1d991672c1b9396e7590789a2f5a0633ae
Sebastian Arcila Valenzuela <sebastianarcila@gmail.com>
2011-06-09 06:11:47 -0500

    euclidian distance to find closest projects

--- app/models/similarity_search.rb
+++ app/models/similarity_search.rb
@@ -1,4 +1,20 @@
-class SimilaritySearch
-  CATEGORIES = [CostScore , TechnologyScore, DevelopmentScore,
-                LegalScore, RiskScore      , OpportunityScore]
+module SimilaritySearch
+  module Math
+    
+    def self.distance(pointa, pointb)
+      ::Math.sqrt((pointa - pointb)**2)
+    end
+    
+  end
+
+  def self.similars(to, tol=10)
+    base_score = to.score
+    nears = []
+    Project.all.each do |p|
+      if p.score and to.id != p.id and Math.distance(base_score, p.score) < tol
+        nears << p
+      end
+    end
+    nears
+  end
 end
