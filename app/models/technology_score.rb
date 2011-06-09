8c07fe45184f88af3331818cba3aa875aa029ebe
Alejandro Peláez <alejopelaezr@gmail.com>
2011-06-08 21:07:33 -0500

    Some style, but no so good :S

--- app/models/technology_score.rb
+++ app/models/technology_score.rb
@@ -4,6 +4,8 @@ class TechnologyScore < ActiveRecord::Base
 
   attr_protected
 
+  scope :by_project, lambda{|q| where(:project_id => q)}
+
   def avg
     (tech_requirements+actual_resources+missing_resources)/3.0
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110608021622) do
+ActiveRecord::Schema.define(:version => 20110609004701) do
 
   create_table "assignments", :force => true do |t|
     t.integer  "project_id"
@@ -95,6 +95,8 @@ ActiveRecord::Schema.define(:version => 20110608021622) do
     t.string   "rfp_content_type"
     t.integer  "rfp_file_size"
     t.datetime "rfp_updated_at"
+    t.string   "comment"
+    t.string   "company"
   end
 
   create_table "risk_scores", :force => true do |t|
